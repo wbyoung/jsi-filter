64e5e6efbfbe03937ff614d309ac8769aa1e9a1d
Dmitry White <white.dmitry@gmail.com>
2014-05-22 14:37:47 -0700

    Local changes by Dmitry

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,27 @@
 var expect = require('chai').expect;
-var filter = require('..').filter;
+var filter = require('../index');
 var map = require('..').map;
 
 describe('filter()', function() {
   it('works for numbers', function() {
     var numbers = [1, 2, 3, 4, 5, 6];
-    var even = filter(numbers, function(n) {
+    var even = filter.filter(numbers, function(n) {
       return (n % 2) === 0;
     });
     expect(even).to.eql([2, 4, 6]);
   });
+
+  it('works for people', function() {
+    var people = ['adam', 'carlo', 'tian', 'ariel', 'grant', 'dmitry', 'sam', 'whitney', 'josh'];
+    var beginsWithA = filter.filter(people, function(name) {
+      return (name.slice(0,1) === 'a');
+    });
+    expect(beginsWithA).to.eql([ 'adam', 'ariel' ]);
+  });
 });
+
+/**
+  * I am not using this function so I am commenting out the test:
 
 describe('map()', function() {
   it('works for people', function() {
@@ -28,3 +39,5 @@ describe('map()', function() {
     expect(names).to.eql(['Sam', 'Carlo', 'Adam', 'Josh']);
   });
 });
+
+*/
